3a7a42e4bd40afc72fb2b84faa1378dcaf3f1b9b
Luiz Henrique <luizhen428@gmail.com>
2025-02-04 16:02:39 -0300

    commit hashing and object

--- CsharpGit/Commands/CommitCommand.cs
+++ CsharpGit/Commands/CommitCommand.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using CSharpGit.Exceptions;
 using CSharpGit.Services;
 using CSharpGit.Utils;
@@ -6,6 +7,16 @@ namespace CSharpGit.Commands;
 
 public class CommitCommand
 {
+
+    private static string BuildCommit(string message, string tree)
+    {
+        var hash = ShaHashService.GenerateHash(DateTimeOffset.Now + "Commiter");
+        var buffer = $"tree {tree}\nAuthor: Commiter\n{message}";
+        var bytes = Encoding.UTF8.GetBytes(buffer);
+        var obj = ObjectService.WriteObject(hash, bytes);
+
+        return hash;
+    }
     public static async ValueTask Commit(string? message)
     {
         if (message is null)
@@ -14,8 +25,8 @@ public class CommitCommand
         if(directory is null)
             throw new RepositoryNotFoundException("Repository not found");
         var tree = await TreeService.IndexTree();
-        await TreeService.BuildTree("root", tree);
-
+        var treeHash = await TreeService.BuildTree("root", tree);
+        var hash = BuildCommit(message, treeHash);
         await IndexUtils.ClearIndexFile(directory);
     }
 }
